328deb73333772c5341a42510e76fef6bac20de1
kamrul Hasan <kamrul.h456@gmail.com>
2022-05-13 23:42:43 +0600

    All model Created

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user','name','email']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','digital']
+admin.site.register(models.Customer,CustomerAdmin)
+admin.site.register(models.Order)
+admin.site.register(models.Product,ProductAdmin)
+admin.site.register(models.OrderItem)
+admin.site.register(models.ShippingAddress)
